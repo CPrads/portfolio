3ae57e050be44af57697287f4af217d7efbfcc55
unknown <carlo.23.prads@gmail.com>
2023-08-11 11:23:24 +0800

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/logo.png'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Welcome to My Portfolio</h1>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Footer from '../components/footer'
+import Navbar from '../components/navbar'
+import Images from '../components/images'
+
+const home = () => {
+  return (
+    <div>
+        <Footer/>
+    </div>
+  )
+}
+
+export default home
